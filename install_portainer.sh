c76485e290e1299c4574c916f9729183b4550489
tmerenciano <theo.merenciano@gmail.com>
2024-11-12 22:38:58 +0100

    [ADD] Portainer and docker install

---
+++ install_portainer.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+# Step 1 : Check linux distro
+if [ "$(uname -s)" = "Linux" ]; then
+    if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    os=$ID
+    echo "$os os detected" 
+        #Step 2 : Install docker
+
+
+        if [ "$os" == "ubuntu" ]; then
+
+                echo "Installation de docker en cours"
+                # Add Docker's official GPG key:
+                sudo apt-get update
+                sudo apt-get install ca-certificates curl
+                sudo install -m 0755 -d /etc/apt/keyrings
+                sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+                sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+                # Add the repository to Apt sources:
+                echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt-get update
+
+                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                sudo docker run hello-world
+        elif [ "$os" == "debian" ]; then
+            echo "Installation de docker en cours"
+            #Add Docker's official GPG key:
+            sudo apt-get update
+            sudo apt-get install ca-certificates curl
+            sudo install -m 0755 -d /etc/apt/keyrings
+            sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+            sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+            # Add the repository to Apt sources:
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt-get update
+
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+            sudo docker run hello-world
+        fi
+    fi
+fi
+
+#Step 2 : Install Portainer
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.4
+docker ps
